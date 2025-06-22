6a675f1d446b4e6789ef0fcd9434eb7797cc6bf6
Nalzurin <alexnormandi@ukr.net>
2025-06-22 21:16:09 +0300

    Make permanent enemy factions able to spawn

---
+++ 1.6/Source/Patch_QuestNode_Root_DistressCall_FactionUsable.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using RimWorld.QuestGen;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BetterDistressCall
+{
+    [HarmonyPatch]
+    public static class Patch_QuestNode_Root_DistressCall_FactionUsable
+    {
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(QuestNode_Root_DistressCall), "FactionUsable");
+        }
+        public static void Postfix(ref bool __result, Faction f, float points)
+        {
+            if (!__result)
+            {
+                if (ModsConfig.RoyaltyActive && points < 2000f && f == Faction.OfEmpire)
+                {
+                    return;
+                }
+                if (!f.def.canGenerateQuestSites)
+                {
+                    return;
+                }
+                if (f.def.humanlikeFaction && !f.def.pawnGroupMakers.NullOrEmpty())
+                {
+                    __result = true;
+                }
+                return;
+            }
+
+        }
+    }
+
+}
